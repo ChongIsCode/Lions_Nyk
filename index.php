fd74e8e344d113d1f5fc395978e170122c5a8ba2
Chong <simonjulendal1201@gmail.com>
2021-03-19 12:00:42 +0100

    Carousel added

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<meta name="author" content="Udgiver">
 	<meta name="copyright" content="Information om copyright">
 	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 	<link href="css/styles.css" rel="stylesheet" type="text/css">
 	
@@ -17,7 +18,9 @@
 
 <body>
 	<!-- Menu -->
-	<?php include 'include/menu.php';?>
+<?php include 'include/menu.php';?>
+
+<?php include 'include/carousel.php';?>
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
