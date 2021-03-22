0891818484df27f1acbc1c74ca04223127d9517f
Bjaef <seanillner@gmail.com>
2021-03-22 12:04:34 +0100

    Tilføjede sec1
    pull mekanik dum

--- omosindex.php
+++ omosindex.php
@@ -26,73 +26,74 @@
             <img src="images/lions.png" class="img-fluid" alt="Lions medlemmer smiler sødt :-)">
         </div>
     </div>
-    <div class="header">
-        <h1>Om Lions Nykøbing</h1>
-    </div>
-    <div class="body">
-        <p>
-            Lions Clubs blev oprettet i USA i 1917 på en tid, hvor mange syntes, at verden behøvede medleven og mindre selvoptagethed.
-            <br>
-            Lions kom derefter til Danmark i 1950, og i d. 02. maj 1964 blev Lions Nykøbing oprettet.
-        </p>
-    </div>
-    <div class="quote">
-        Lions International sørger for at hjælpe dem der ikke kan hjælpe sig selv.
-    </div>
-    <div class="header">
-        <h1>Lions hjælper</h1>
-    </div>
-    <div class="body">
-        <p>
-            Der er mange måde Lions hjælper på. Dette kan blandt andet være indsamlinger til kræft, covid-19
-            eller folk til jul, der ikke har råd til julegaver eller mad.
-            <br>
-            Lions engagerer sig meget i samfundet, dog ikke politisk eller religiøst og har tilsluttet sig FN's
-            deklaration om menneskerettigheder og arbejder for samfundsforståelse og fred.
-        </p>
-    </div>
-    <div class="quote">
-        Lions blev i 2007 kåret af Financial Times som den mest effektive Non Governmental Organization.
-    </div>
-    <div class="body">
-        <p>
-            I Lions Danmark donerer vi årligt omkring 30-40 millioner kroner til humanitære formål.
-            Pengene fordeles suverænt af den enkelte klub og går både til
-            internationale, nationale og lokale formål i nærområdet.
-        </p>
-    </div
-    <div class="header">
-        <h1>Hvor findes Lions?</h1>
-    </div>
-    <div class="figure">
-        <div class="figure__image">
-            <img src="images/lionsmap.png" class="img-fluid" alt="Lions verdenskort">
+    <div class="sec1">
+        <div class="header">
+            <h1>Om Lions Nykøbing</h1>
         </div>
+        <div class="body">
+            <p>
+                Lions Clubs blev oprettet i USA i 1917 på en tid, hvor mange syntes, at verden behøvede medleven og mindre selvoptagethed.
+                <br>
+                Lions kom derefter til Danmark i 1950, og i d. 02. maj 1964 blev Lions Nykøbing oprettet.
+            </p>
+        </div>
+        <div class="quote">
+            Lions International sørger for at hjælpe dem der ikke kan hjælpe sig selv.
+        </div>
+        <div class="header">
+            <h1>Lions hjælper</h1>
+        </div>
+        <div class="body">
+            <p>
+                Der er mange måde Lions hjælper på. Dette kan blandt andet være indsamlinger til kræft, covid-19
+                eller folk til jul, der ikke har råd til julegaver eller mad.
+                <br>
+                Lions engagerer sig meget i samfundet, dog ikke politisk eller religiøst og har tilsluttet sig FN's
+                deklaration om menneskerettigheder og arbejder for samfundsforståelse og fred.
+            </p>
+        </div>
+        <div class="quote">
+            Lions blev i 2007 kåret af Financial Times som den mest effektive Non Governmental Organization.
+        </div>
+        <div class="body">
+            <p>
+                I Lions Danmark donerer vi årligt omkring 30-40 millioner kroner til humanitære formål.
+                Pengene fordeles suverænt af den enkelte klub og går både til
+                internationale, nationale og lokale formål i nærområdet.
+            </p>
+        </div
+        <div class="header">
+            <h1>Hvor findes Lions?</h1>
+        </div>
+        <div class="figure">
+            <div class="figure__image">
+                <img src="images/lionsmap.png" class="img-fluid" alt="Lions verdenskort">
+            </div>
+        </div>
+        <ul class="list">
+            <li class="list__item">- Lions oprettet i 1917 i USA</li>
+            <li class="list__item">- Lions kom til Danmark i 1950</li>
+            <li class="list__item">- Lions Nykøbing charteret i 1964</li>
+            <li class="list__item">- I dag er der cirka 45.000 klubber</li>
+            <li class="list__item">- Lions er til stede i mere end 200 lande</li>
+            <li class="list__item">- Hermed typisk 20-40 medlemmer</li>
+        </ul>
+        <hr>
+        <div class="header">
+            <h1>Etik og formål?</h1>
+        </div>
+        <ul class="list--ethics">
+            <li class="list__item">> at skabe og opretholde en atmosfære af forståelse mellem verdens folk</li>
+            <li class="list__item">> at fremme principperne om en redelig ledelse af samfundet og godt medborgerskab</li>
+            <li class="list__item">> at være aktivt interesseret i samfundets medborgerlige, kulturelle, sociale og moralske velfærd</li>
+            <li class="list__item">> at forene klubberne i venskab, godt kammeratskab og gensidig forståelse</li>
+            <li class="list__item">> at danne et forum til fri drøftelse af alle problemer af almen interesse dog således,
+                at klubbernes medlemmer ikke bør diskutere partipolitik og trosbekendelser</li>
+            <li class="list__item">> at opfordre personer til gennem deres medlemskab at tjene samfundet uden personlig
+                økonomisk vinding, at fremme effektivitet og en høj etisk standard i handel, industri, i faglig sammenhæng,
+                samt i offentlig og privat virksomhed</li>
+        </ul>
     </div>
-    <ul class="list">
-        <li class="list__item">- Lions oprettet i 1917 i USA</li>
-        <li class="list__item">- Lions kom til Danmark i 1950</li>
-        <li class="list__item">- Lions Nykøbing charteret i 1964</li>
-        <li class="list__item">- I dag er der cirka 45.000 klubber</li>
-        <li class="list__item">- Lions er til stede i mere end 200 lande</li>
-        <li class="list__item">- Hermed typisk 20-40 medlemmer</li>
-    </ul>
-    <hr>
-    <div class="header">
-        <h1>Etik og formål?</h1>
-    </div>
-    <ul class="list--ethics">
-        <li class="list__item">> at skabe og opretholde en atmosfære af forståelse mellem verdens folk</li>
-        <li class="list__item">> at fremme principperne om en redelig ledelse af samfundet og godt medborgerskab</li>
-        <li class="list__item">> at være aktivt interesseret i samfundets medborgerlige, kulturelle, sociale og moralske velfærd</li>
-        <li class="list__item">> at forene klubberne i venskab, godt kammeratskab og gensidig forståelse</li>
-        <li class="list__item">> at danne et forum til fri drøftelse af alle problemer af almen interesse dog således,
-            at klubbernes medlemmer ikke bør diskutere partipolitik og trosbekendelser</li>
-        <li class="list__item">> at opfordre personer til gennem deres medlemskab at tjene samfundet uden personlig
-            økonomisk vinding, at fremme effektivitet og en høj etisk standard i handel, industri, i faglig sammenhæng,
-            samt i offentlig og privat virksomhed</li>
-    </ul>
-
 </div>
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
