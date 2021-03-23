476d023630d2eb36098ba5abdb73678a9e48e260
Bjaef <seanillner@gmail.com>
2021-03-23 12:37:31 +0100

    Næsten færdiggjorde bliv medlem side

--- medlemindex.php
+++ medlemindex.php
@@ -37,25 +37,188 @@
             </p>
         </div>
         <div class="header">
-            <h2>Lions hjælper</h2>
+            <h2>Info</h2>
         </div>
-        <div class="body">
+        <section class="formula">
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>Fornavn</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="firstname" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>Efternavn</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="surname" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <br>
+            <br>
+
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>Adresse</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="address" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>Postnummer</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="postal" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>By</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="city" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <br>
+            <br>
+
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>Email</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="email" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+            <div class="formula__form">
+                <div class="formula__header">
+                    <p>Mobilnummer</p>
+                </div>
+                <div class="input-group input-group-sm mb-3">
+                    <input id="tel" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                </div>
+            </div>
+
+            <br>
+            <br>
+
+            <div class="formula__bday">
+                <div class="formula__header">
+                    <p>Fødselsdato</p>
+                </div>
+                <div class="formula__date">
+                    <div class="formula__input">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>dd</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                            <option value="13">13</option>
+                            <option value="14">14</option>
+                            <option value="15">15</option>
+                            <option value="16">16</option>
+                            <option value="17">17</option>
+                            <option value="18">18</option>
+                            <option value="19">19</option>
+                            <option value="20">20</option>
+                            <option value="21">21</option>
+                            <option value="22">22</option>
+                            <option value="23">23</option>
+                            <option value="24">24</option>
+                            <option value="25">25</option>
+                            <option value="26">26</option>
+                            <option value="27">27</option>
+                            <option value="28">28</option>
+                            <option value="29">29</option>
+                            <option value="30">30</option>
+                            <option value="31">31</option>
+                        </select>
+                    </div>
+                    <div class="formula__input">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>mm</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            <option value="11">11</option>
+                            <option value="12">12</option>
+                        </select>
+                    </div>
+                    <div class="formula__input--wide">
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>åååå</option>
+                            <option value="1">2021</option>
+                            <option value="2">2020</option>
+                            <option value="3">2019</option>
+                            <option value="4">2018</option>
+                            <option value="5">2017</option>
+                            <option value="6">2016</option>
+                            <option value="7">2015</option>
+                            <option value="8">2014</option>
+                            <option value="9">2013</option>
+                            <option value="10">2012</option>
+                            <option value="11">2011</option>
+                            <option value="12">2010</option>
+                            <option value="13">2009</option>
+                            <option value="14">2008</option>
+                            <option value="15">2007</option>
+                            <option value="16">2006</option>
+                            <option value="17">2005</option>
+                            <option value="18">2004</option>
+                            <option value="19">2003</option>
+                            <option value="20">2002</option>
+                            <option value="21">2001</option>
+                            <option value="22">2000</option>
+                            <option value="23">1999</option>
+                            <option value="24">1998</option>
+                            <option value="25">1997</option>
+                            <option value="26">1996</option>
+                            <option value="27">1995</option>
+                            <option value="28">1994</option>
+                            <option value="29">1993</option>
+                            <option value="30">1992</option>
+                            <option value="31">1991</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="terms">
+            <h2>
+                Vilkår og betingelser
+            </h2>
             <p>
-                Der er mange måde Lions hjælper på. Dette kan blandt andet være indsamlinger til kræft, covid-19
-                eller folk til jul, der ikke har råd til julegaver eller mad.
-                <br>
-                <br>
-                Lions engagerer sig meget i samfundet, dog ikke politisk eller religiøst og har tilsluttet sig FN's
-                deklaration om menneskerettigheder og arbejder for samfundsforståelse og fred.
+                Læs venlight vores vilkår og betingelser før, at du melder dig ind.
             </p>
         </div>
-        <div class="body">
-            <p>
-                I Lions Danmark donerer vi årligt omkring 30-40 millioner kroner til humanitære formål.
-                Pengene fordeles suverænt af den enkelte klub og går både til
-                internationale, nationale og lokale formål i nærområdet.
-            </p>
-        </div
+        <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+                Jeg har læst og accepterer vilkår- og betingelserne
+            </label>
+        </div>
     </section>
     <div class="backtotop">
         SEJ KNAP HER
