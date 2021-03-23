399a229199f81a451cb85f642b85f5c8704a5def
Bjaef <seanillner@gmail.com>
2021-03-23 09:36:17 +0100

    Rettede skrifttype og oprettede ny side

---
+++ medlemindex.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Sigende titel</title>
+	
+	<meta name="robots" content="All">
+	<meta name="author" content="Udgiver">
+	<meta name="copyright" content="Information om copyright">
+	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<!-- Menu -->
+<?php include 'include/menu.php';?>
+
+<div class="OmOs">
+    <section class="sec1">
+        <div class="header">
+            <h1>Bliv medlem</h1>
+        </div>
+        <div class="body">
+            <p>
+                Ved at melde dig ind i Lions Nykøbing, kan du gøre en forskel i Guldborgsund Kommune.
+                <br>
+                <br>
+                Som medlem af Lions vil du betale kontigent på 99, kr om måneden. Pengene går ubeskåret til
+                humanitære formål og intet af medlemsbidraget går til administration eller mødeudgifter.
+            </p>
+        </div>
+        <div class="header">
+            <h2>Lions hjælper</h2>
+        </div>
+        <div class="body">
+            <p>
+                Der er mange måde Lions hjælper på. Dette kan blandt andet være indsamlinger til kræft, covid-19
+                eller folk til jul, der ikke har råd til julegaver eller mad.
+                <br>
+                <br>
+                Lions engagerer sig meget i samfundet, dog ikke politisk eller religiøst og har tilsluttet sig FN's
+                deklaration om menneskerettigheder og arbejder for samfundsforståelse og fred.
+            </p>
+        </div>
+        <div class="body">
+            <p>
+                I Lions Danmark donerer vi årligt omkring 30-40 millioner kroner til humanitære formål.
+                Pengene fordeles suverænt af den enkelte klub og går både til
+                internationale, nationale og lokale formål i nærområdet.
+            </p>
+        </div
+    </section>
+    <div class="backtotop">
+        SEJ KNAP HER
+    </div>
+</div>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
