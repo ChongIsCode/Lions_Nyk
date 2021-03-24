8aeb18177147f96592cf462cfe87c107ca5ff346
Bjaef <seanillner@gmail.com>
2021-03-24 15:46:42 +0100

    Bliv sponsor side færdig

---
+++ blivsponsorindex.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+	<meta charset="utf-8">
+	
+	<title>Bliv sponsor</title>
+	
+	<meta name="robots" content="All">
+	<meta name="author" content="Udgiver">
+	<meta name="copyright" content="Information om copyright">
+	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="css/styles.css" rel="stylesheet" type="text/css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<!-- Menu -->
+<?php include 'include/menu.php';?>
+
+<div class="OmOs">
+    <section class="sec1">
+        <div class="h1--special">
+            <h1>Bliv sponsor</h1>
+        </div>
+        <div class="h2--special">
+            <h2>Hvad får din virksomhed ud af at være sponsor for Lions Nykøbing?</h2>
+            <div class="header__line">
+            </div>
+        </div>
+        <div class="body">
+            <p>
+                Som sponsor for Lions Nykøbing får du ikke mindst god reklame for din forretning, men du er også med
+                i kampen om at skabe en bedre verden. Din virksomhed vil blive aktivt markedsført, og derved skabe
+                mere synlighed og reklameværdi, samtidigt med at du støtter et godt formål.
+                <br>
+                <br>
+                Hos Lions Nykøbing er vi altid glade for aktive sponsorere, og er der altid plads til flere.
+                <br>
+                <br>
+                Du er velkommen til at udfylde formularen nedenfor eller kontakte os telefonisk på nummeret nedenfor.
+                <br>
+                <br>
+                <span class="p--bold">+45 6547 0978</span>
+                <br>
+                <br>
+                Din hjælp gør en forskel.
+            </p>
+        </div>
+        <section class="input">
+            <div class="header">
+                <h2>Info</h2>
+            </div>
+            <section class="formula">
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Fornavn</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="firstname" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Efternavn</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="surname" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+
+                <br>
+                <br>
+
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Virksomheds navn</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="address" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Virksomheds adresse</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="postal" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Virksomheds postnummer</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="city" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Email</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="city" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Mobilnummer</p>
+                    </div>
+                    <div class="input-group input-group-sm mb-3">
+                        <input id="city" type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                    </div>
+                </div>
+                <div class="formula__form">
+                    <div class="formula__header">
+                        <p>Eventuel note</p>
+                    </div>
+                    <div class="input-group">
+                        <textarea class="form-control" aria-label="With textarea"></textarea>
+                    </div>
+                </div>
+        </section>
+    </section>
+    <div class="page-button">
+        <button type="button" class="btn btn-primary btn-lg">Fortsæt</button>
+    </div>
+    </div>
+
+<!-- Footer -->
+<?php include 'include/footer.php';?>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
